17bea8b27ed3ed6f3cc39592f33cad33cc3f9fbc
navyabshetty8-ops <navyabshetty8@gmail.com>
2025-09-04 12:48:24 +0530

    Update Welcome.java

--- gitdemo/src/gitdemo/Welcome.java
+++ gitdemo/src/gitdemo/Welcome.java
@@ -5,7 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git");
 		System.out.println("Pushing the code to github");
-		System.out.println("Update the code to github");
+		System.out.println("Pulling the code to github");
 		System.out.println("pushing the code to remote: attempt2");
 		System.out.println("pushing the code");
 		System.out.println("pushing to branch1");
