5492bc2b39a2b734d096a722e13ab80c2731bf8e
navyabshetty8-ops <navyabshetty8@gmail.com>
2025-09-04 12:16:46 +0530

    Update Welcome.java

--- gitdemo/src/gitdemo/Welcome.java
+++ gitdemo/src/gitdemo/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git");
-		System.out.println("Pushing the code to github");
+		System.out.println("Update the code to github");
 	}
 
 }
